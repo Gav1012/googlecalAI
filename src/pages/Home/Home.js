913308021476e9d8ff6812e5e36ece69651985c5
Gav1012 <gavin.poley@gmail.com>
2024-02-05 20:34:36 -0800

    added css and updated to UI to not destroy my eyes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react"
+import './Home.css'
 
 function Home() {
     return (
-        <div className="home-wrapper">
+        <div className="home-wrapper" style={{color: "white"}}>
             Welcome to GoogleCalendarAI (WIP of course)
             <input type="text" placeholder="Enter you prompt"></input>
+            <button className="generate-button">Generate</button>
         </div>
     );
 }
