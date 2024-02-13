f34a41d2785c8c51fd85b31ac37961d8739f66b8
Gav1012 <gavin.poley@gmail.com>
2024-02-12 19:47:35 -0800

    updated ai file to use api key, can get responses

--- src/ai.py
+++ src/ai.py
@@ -1,6 +1,11 @@
+import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
-client = OpenAI()
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
