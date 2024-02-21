2d7c2079363db7dc6f381225c3837e0fa068e405
Gav1012 <gavin.poley@gmail.com>
2024-02-21 12:16:35 -0800

    fixed data not being passed between py and js files

---
+++ ai.py
@@ -0,0 +1,32 @@
+# import os
+# from openai import OpenAI
+# from dotenv import load_dotenv
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+# load_dotenv()
+
+# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+# completion = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+#     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+#   ]
+# )
+
+# print(completion.choices[0].message)
+# message = completion.choices[0].message
+
+@app.route("/get_message")
+def get_message():
+  # message = "Hello from ai.py"
+  # return jsonify(message=message)
+  return {"members": ["Member1, Member2, Member3"]}
+
+if __name__ == '__main__':
+  app.run(debug=True)
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './Home.css'
 
-fetch('/get_message')
-    .then(response => response.json())
-    .then(data => {
-        console.log('recieved data: ', data.message);
-    });
-
 function Home() {
+    const [message, setMessage] = useState([{}])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/get_message')
+        .then(res => res.json())
+        .then(data => {
+            setMessage(data);
+            console.log(data);
+        });
+    }, []);
+
     return (
         <div className="home-wrapper" style={{color: "white"}}>
             <button className="login-button">Login</button>
             <h1>Welcome to GoogleCalendarAI (WIP of course)</h1>
             <textarea type="text" className="input-wrapper" placeholder="Enter your calendar details"></textarea>
             <button className="generate-button">Generate</button>
+            <div>{message.members}</div>
         </div>
     );
 }
