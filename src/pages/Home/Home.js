4a6b895ab45c773ac1734a3fc949f509d903f289
Gav1012 <gavin.poley@gmail.com>
2024-02-22 23:02:20 -0800

    added functions to work with flask and submitting user input

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react"
 import './Home.css'
 
 function Home() {
-    const [message, setMessage] = useState([{}])
+    // holds the ChatGPT response from ai.py
+    const [message, setMessage] = useState([{}]);
+    const [userInput, setUserInput] = useState('');
 
+    // uses the /get_message route and sets to messsage state
     useEffect(() => {
         fetch('http://localhost:5000/get_message')
         .then(res => res.json())
@@ -13,6 +16,26 @@ function Home() {
         });
     }, []);
 
+    // sends the input from the textarea to chatgpt to be generated
+    const sendDatatoFlask = async() => {
+        const response = await fetch('http://localhost:5000/send_input', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data: userInput })
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+    };
+    
+    // handles clicking the submit button to then send input data
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        sendDatatoFlask();
+    }
+
+    // main page react components, will need to update alooot
     return (
         <div className="home-wrapper" style={{color: "white"}}>
             <button className="login-button">Login</button>
