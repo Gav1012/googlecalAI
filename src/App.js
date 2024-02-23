267589fefe1b775e58d6ac46a671baa55b01a6b3
Gav1012 <gavin.poley@gmail.com>
2024-02-22 23:01:05 -0800

    added comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home/Home";
 import './App.css';
 
+// main file to display all pages, may add result and about/contact page
 function App() {
   return (
     <div className="App">
