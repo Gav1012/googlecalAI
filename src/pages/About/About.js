07b413a5d593abda89b3e65b1f2cca6e11234a05
Gav1012 <gavin.poley@gmail.com>
2024-03-30 20:46:38 -0700

    updated about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,6 +2,14 @@ import React from "react";
 
 function About() {
     print("hello about");
+    return (
+        <div>
+            <h1>About/Credits(?)</h1>
+            <div>Program created by: gavin poley</div>
+            <div>Tech Stuff: JS, React, Firebase, OpenAI</div>
+            <div>used internet for fancy css stuff (probably not implemented rn)</div>
+        </div>
+    )
 }
 
 export default About;
