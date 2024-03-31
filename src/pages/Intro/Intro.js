d7be4895650fcc6fc5a89719c3a4fecbf708e289
Gav1012 <gavin.poley@gmail.com>
2024-03-30 20:41:20 -0700

    added basic intro page

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Intro() {
-    print("hello intro");
+    return(
+        <div>
+            <h1>Introduction/How to Use</h1>
+            <div>this page will explain the program usage and overall</div>
+        </div>
+    )
+    
 }
 
 export default Intro;
