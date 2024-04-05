2ce3473680cda70c658a2a9db7dda3a3d8d6f7e3
Gav1012 <gavin.poley@gmail.com>
2024-04-04 23:46:12 -0700

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Home from "./pages/Home/Home";
-import './App.css';
+import About from './pages/About/About';
+import Intro from "./pages/Intro/Intro";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // main file to display all pages, may add result and about/contact page
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path='/about' element={<About/>}></Route>
+          <Route exact path='/intro' element={<Intro/>}></Route>
+        </Routes> 
+      </Router>
+      
     </div>
   );
 }
