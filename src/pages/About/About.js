5ba19cc643406cfb9a5977e911d175b37bb15a31
Gav1012 <gavin.poley@gmail.com>
2024-04-08 22:14:06 -0700

    added temp links for About and Intro, will update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import "About.css";
 
 function About() {
     return (
         <div className="about-wrapper" style={{color: "white"}}>
+            <Link
+                className={"home-nav"}
+                to={"/"}
+                style={{textDecoration: "none"}}
+            >
+                Home
+            </Link>
+            <Link
+                className={"home-nav"}
+                to={"/intro"}
+                style={{textDecoration: "none"}}
+            >
+                Intro
+            </Link>
+            <Link
+                className={"home-nav"}
+                to={"/about"}
+                style={{textDecoration: "none"}}
+            >
+                About
+            </Link>
             <h1>About/Credits(?)</h1>
             <div>Program created by: gavin poley</div>
             <div>Tech Stuff: JS, React, Firebase, OpenAI</div>
--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import "Intro.css";
 
 function Intro() {
     return(
         <div className="intro-wrapper" style={{color: "white"}}>
+            <Link
+                className={"home-nav"}
+                to={"/"}
+                style={{textDecoration: "none"}}
+            >
+                Home
+            </Link>
+            <Link
+                className={"home-nav"}
+                to={"/intro"}
+                style={{textDecoration: "none"}}
+            >
+                Intro
+            </Link>
+            <Link
+                className={"home-nav"}
+                to={"/about"}
+                style={{textDecoration: "none"}}
+            >
+                About
+            </Link>
             <h1>Introduction/How to Use</h1>
             <div>this page will explain the program usage and overall</div>
         </div>
